51b38b2c77aed9ca21a67006b671a4bb8f576bab
Juste_ozan <117074595+osahinGuardia@users.noreply.github.com>
2023-02-28 10:45:36 +0100

    Amélioration de la grille

--- sudoku.c
+++ sudoku.c
@@ -6,19 +6,36 @@
 
 //creation de la grille :
 
-void ft_grille(largeur, hauteur){
+void ft_grille(int largeur, int hauteur){
 
-    int i = 0, j = 0;
-    for(i=0; i<largeur; i++){
+    int i, j, k, l;
+    for(i=0; i<4; i++){
         printf("\n");
-        for(j=0; j<hauteur; j++){
-
-            print("*");
+        
+        for(j=0; j<3; j++)
+            printf("|---");
+        
+        l = 0;
+        for(k=0; k<3; k++){
+            
+            for(l; l<1; l++)
+                printf("|\n");
+            
+            printf("|   ");
         }
+        printf("|");
+       
     }
+    printf("\n");
+    for(j=0; j<3; j++)
+        printf("|---");
+        
+    printf("|"); 
 }
 
 int main(int argc, char *argv){
 
+    argv[1] = 3;
+    argv[2] = 4;
     ft_grille(argv[1], argv[2]);
 }
